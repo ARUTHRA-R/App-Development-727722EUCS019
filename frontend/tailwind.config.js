d0128321c4252c25c3544762f97723fdfd6847a2
ARUTHRA-R <rajesh.aruthra@gmail.com>
2024-08-13 10:40:47 +0530

    commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
+  darkMode: ['class'],
+  
+  theme: {
+    // Customize Tailwind's theme based on your design requirements
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      mono: ['Kode Mono'],
+      madimi: ['Madimi One', 'sans-serif']
+    },
+    colors: {
+      'regal-blue': '#243c5a',
+      'churn': '#01c293',
+      'purple': { 
+        800: '#6B46C1', 
+      },
+    },
+  },
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('flowbite/plugin'),
+  ],
+};
+
